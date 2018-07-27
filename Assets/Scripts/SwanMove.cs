073fa684938a00312f5a2a2efd18de8b7a5f8cdb
verehu <huweigoodboy@126.com>
2018-07-27 21:58:20 +0800

    swan commit

---
+++ Assets/Scripts/SwanMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwanMove : MonoBehaviour {
+    private int moveSpeed = 4;  //swan移动速度
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(22, 3, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x > -22) {
+            transform.Translate(transform.right  *- moveSpeed * Time.deltaTime);
+        } else {
+            transform.position = new Vector3(22, 3, 0);
+        }
+	}
+}
